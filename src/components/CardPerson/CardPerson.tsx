ad7c7b0bc65b923e3dd7819aac7cd974a962362e
anpdko <anpdko@gmail.com>
2023-06-29 17:28:55 +0300

    respon team page

--- src/components/CardPerson/CardPerson.tsx
+++ src/components/CardPerson/CardPerson.tsx
@@ -5,36 +5,43 @@ import decorImg from '../../assets/images/team/decor.png'
 import frameImg from '../../assets/images/team/frame.png'
 import IconInst from '../../components/UI/IconInst/IconInst'
 
-function getRndInteger(min:number, max:number):number {
-   return Math.floor(Math.random() * (max - min) ) + min;
- }
+function getRndInteger(min: number, max: number): number {
+   return Math.floor(Math.random() * (max - min)) + min;
+}
 
-const CardPerson = ({data}:{data:ITeam}) => {
+const CardPerson = ({ data }: { data: ITeam }) => {
    const sideDecor = Math.floor(Math.random() * 2)
    const angleCard = getRndInteger(-7, 7)
 
    return (
-      <div 
-      className={[s.card, sideDecor?s.left:s.right].join(" ")}
-      style={{transform: "rotate(" + angleCard + "deg)"}}
-      >
-         <div className={s.box_frame}>
-            <img className={s.frame} src={frameImg} alt="" />
-            <div className={s.box_foto}>
-               <img className={s.foto} src={data.foto}
-                  alt="" />
-               <h2 className={s.name}>{data.name}</h2>
+      <div className={s.box_card}>
+         <div
+            className={[s.card, sideDecor ? s.left : s.right].join(" ")}
+            style={{ transform: "rotate(" + angleCard + "deg)" }}
+         >
+            <div className={s.box_frame}>
+               <img className={s.frame} src={frameImg} alt="" />
+               <div className={s.box_foto}>
+                  <img className={s.foto} src={data.foto}
+                     alt="" />
+                  <h2 className={s.name}>{data.name}</h2>
+               </div>
+               <img className={s.decor} src={decorImg} alt='decor' />
+               {data?.inst &&
+                  <IconInst link={data.inst} className={s.inst} />
+               }
+               <div className={s.messages}>
+                  {data.discription}
+               </div>
             </div>
-            <img className={s.decor} src={decorImg} alt='decor' />
-            { data?.inst && 
-               <IconInst link={data.inst} className={s.inst}/>
-            }
-            <div className={s.messages}>
-               {data.discription}
-            </div>
+            <p className={s.job}>{data.job}</p>
          </div>
-         <p className={s.job}>{data.job}</p>
+
+         <div className={s.mob_messages}>
+            {data.discription}
+         </div>
       </div>
+
    );
 };
 export default CardPerson
