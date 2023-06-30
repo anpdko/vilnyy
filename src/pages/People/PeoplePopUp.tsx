b3aaa76ac757dd77ba5f8bd2d4f406bc949e37fd
anpdko <anpdko@gmail.com>
2023-07-01 00:34:30 +0300

    popup

--- src/pages/People/PeoplePopUp.tsx
+++ src/pages/People/PeoplePopUp.tsx
@@ -3,26 +3,26 @@ import s from './People.module.scss'
 import { Link } from 'react-router-dom';
 import NoScrollContainer from '../../components/NoScrollContainer/NoScrollContainer'
 
-import {IPerson} from '../../data/people'
+import { IPerson } from '../../data/people'
 
-const PeoplePopUp = ({person}:{ person: IPerson }) => {
+const PeoplePopUp = ({ person }: { person: IPerson }) => {
    return (
       <NoScrollContainer>
-      <div className={s.bg_popup}>
-         <div className={s.box_popup}>
-            <Link className={s.close} to="/colivers">
-               <i className="bi bi-x-lg"></i>
-            </Link>
-            <h2 dangerouslySetInnerHTML={{ __html: person.title }}></h2>
-            <div className={s.col}>
-               <div className={s.body}>
-                  <p dangerouslySetInnerHTML={{ __html: person.body }}></p>
+         <div className={s.bg_popup}>
+            <div className={s.box_popup}>
+               <Link className={s.close} to="/colivers">
+                  <i className="bi bi-x-lg"></i>
+               </Link>
+               <h2 dangerouslySetInnerHTML={{ __html: person.title }}></h2>
+               <div className={s.col}>
+                  <div className={s.body}>
+                     <p dangerouslySetInnerHTML={{ __html: person.body }}></p>
+                  </div>
+                  <img className={s.foto_card} src={person.fotoCard} alt="foto-card" />
                </div>
-               <img className={s.foto_card} src={person.fotoCard} alt="foto-card" />
             </div>
          </div>
-      </div>
-   </NoScrollContainer>
+      </NoScrollContainer>
    );
 };
 export default PeoplePopUp
