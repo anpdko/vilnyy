8582cde1a58e711d20cef93320c7149d84916805
anpdko <anpdko@gmail.com>
2023-07-03 14:39:31 +0300

    fix

--- src/pages/People/PeoplePopUp.tsx
+++ src/pages/People/PeoplePopUp.tsx
@@ -1,27 +1,29 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import s from './People.module.scss'
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import NoScrollContainer from '../../components/NoScrollContainer/NoScrollContainer'
 import { IPerson } from '../../data/people'
 import gsap from 'gsap'
+import IconInst from '../../components/UI/IconInst/IconInst';
 
 const PeoplePopUp = ({ person }: { person: IPerson }) => {
    const navigate = useNavigate();
 
-   useEffect(()=>{
-      gsap.from(`.${s.bg_popup}`, { delay:0.2, duration: 0.2, opacity: 0, ease: 'power3.easeIn' })
-      gsap.from(`.${s.box_popup}`, { delay:0, duration: 0.7, opacity: 0, scale: 0, ease: 'power3.easeIn' })
+   useEffect(() => {
+      gsap.from(`.${s.bg_popup}`, { delay: 0, duration: 0.2, opacity: 0, ease: 'power3.easeIn' })
+      gsap.from(`.${s.box_popup}`, { delay: 0, duration: 0.5, opacity: 0, scale: 0, ease: 'power3.easeIn' })
    }, [])
 
    const handleClose = () => {
-      gsap.to(`.${s.bg_popup}`, { duration: 0.4, opacity: 0, ease: 'power3.easeOut'})
-      gsap.to(`.${s.box_popup}`, { duration: 0.4, opacity: 0, scale: 0, ease: 'power3.easeOut',
-      onComplete: () => {
-         navigate('/colivers');
-      } })
+      gsap.to(`.${s.bg_popup}`, { duration: 0.2, opacity: 0, ease: 'power3.easeOut' })
+      gsap.to(`.${s.box_popup}`, {
+         duration: 0.3, opacity: 0, scale: 0, ease: 'power3.easeOut',
+         onComplete: () => {
+            navigate('/colivers');
+         }
+      })
    };
 
-   
    return (
       <NoScrollContainer>
          <div className={s.bg_popup}>
@@ -29,7 +31,12 @@ const PeoplePopUp = ({ person }: { person: IPerson }) => {
                <button className={s.close} onClick={handleClose}>
                   <i className="bi bi-x-lg"></i>
                </button>
-               <h2 dangerouslySetInnerHTML={{ __html: person.title }}></h2>
+               <h2 className={s.title}>
+                  <span dangerouslySetInnerHTML={{ __html: person.title }}></span>
+                  <div className={s.text_inst}>
+                     {!!person.inst && <IconInst link={person.inst} className={s.inst} />}
+                  </div>
+               </h2>
                <div className={s.col}>
                   <div className={s.body}>
                      <p dangerouslySetInnerHTML={{ __html: person.body }}></p>
